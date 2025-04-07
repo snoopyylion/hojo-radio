f73ae4aaa8d8ab7fdeed9223d7a3619d88966d37
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-04-07 20:47:56 +0100

    updated

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, getUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export default clerkMiddleware(async (auth, req) => {
-  const user = auth.user;
+  const { userId } = auth;
 
-  if (!user) {
+  if (!userId) {
     return; // Not signed in, Clerk will handle redirection
   }
 
-  const role = user.publicMetadata?.role;
+  const user = await getUser(userId);
+  const role = user?.publicMetadata?.role;
 
   if (req.nextUrl.pathname.startsWith('/admin') && role !== 'admin') {
-    return Response.redirect(new URL('/unauthorized', req.url));
+    return NextResponse.redirect(new URL('/unauthorized', req.url));
   }
 
   if (req.nextUrl.pathname.startsWith('/user') && role !== 'user') {
-    return Response.redirect(new URL('/unauthorized', req.url));
+    return NextResponse.redirect(new URL('/unauthorized', req.url));
   }
 });
-
-export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',  // Always run for API routes
-  ],
-};
