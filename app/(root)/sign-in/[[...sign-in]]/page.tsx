c302c2a897a38bcb41d16c337c609aea046fb3f9
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-04-13 09:52:26 +0100

    updated picture

--- app/(root)/sign-in/[[...sign-in]]/page.tsx
+++ app/(root)/sign-in/[[...sign-in]]/page.tsx
@@ -2,15 +2,19 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <SignIn
-        path="/sign-in"
-        routing="path"
-        signUpUrl="/sign-up"
-        afterSignInUrl="/"
-        afterSignUpUrl="/"
+    <div
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: "url('/img/bg-sign2.jpg')" }} // replace with your actual image path
+    >
+      <div className="bg-white bg-opacity-80 backdrop-blur-md rounded-xl shadow-lg">
+        <SignIn
+          path="/sign-in"
+          routing="path"
+          signUpUrl="/sign-up"
+          afterSignInUrl="/"
+          afterSignUpUrl="/sign-in"
         />
-
+      </div>
     </div>
   );
 }
--- app/(root)/sign-up/[[...sign-up]]/page.tsx
+++ app/(root)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,10 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: "url('/img/bg-sign2.jpg')" }} // replace with your actual image path
+    >
       <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
     </div>
   );
