b3369181a764bef0e9e793e62359c92a58fefbf9
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-04-17 19:00:26 +0100

    updated

--- sanity-codegen.config.ts
+++ sanity-codegen.config.ts
@@ -1,7 +1,4 @@
-// sanity-codegen.config.ts
-import { defineConfig } from 'sanity-codegen'; // Ensure this import is correct
-
-export default defineConfig({
-  schemaPath: './sanity/schema', // Path to the folder where your schema files reside
-  outputPath: './types/sanity.types.ts', // Path where the generated types should be saved
-});
+export default {
+  schemaPath: './sanity/schema', // or wherever your schema lives
+  outputPath: './types/sanity.types.ts',
+}
