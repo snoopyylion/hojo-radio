29beaee37474502571d068f9e07cbb221cb3914c
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-04-24 13:53:34 +0100

    updated

--- app/(root)/aboutus/page.tsx
+++ app/(root)/aboutus/page.tsx
@@ -1,8 +1,11 @@
+import OurMission from '@/components/OurMission'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='bg-white dark:bg-black pt-[150px]'>
+      <OurMission/>
+    </div>
   )
 }
 
