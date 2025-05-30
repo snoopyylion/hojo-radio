d6cd4cf9ce68d960c97e896f771fe0efe2dcaecb
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-05-30 17:28:16 +0100

    updated

--- app/api/user/likes/route.ts
+++ app/api/user/likes/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
 import { supabaseAdmin } from "@/lib/supabase/admin";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 export async function GET() {
   try {
