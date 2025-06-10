44656bc59cc594c8fe21586a00e14bacfbda319b
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-06-10 17:51:10 +0100

    update

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import SearchResultsPage from '../../../components/SearchResultPage';
 
 export default function SearchPage() {
-  return <SearchResultsPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchResultsPage />
+    </Suspense>
+  );
 }
