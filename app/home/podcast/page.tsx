9799d95b6468ed037e9dd92651a437d0665d3f98
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-09-13 03:52:55 +0100

    updated

--- app/home/podcast/page.tsx
+++ app/home/podcast/page.tsx
@@ -11,7 +11,7 @@ export default async function PodcastPage() {
   }
 
   const supabase = createClient(
-    process.env.SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 
